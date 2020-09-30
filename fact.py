bf5ae7ebc850e27afa55c79871fc8f8070767aa1
Gorav Agarwal <55653105+Gourav8152-ai@users.noreply.github.com>
2020-10-01 02:36:07 +0530

    Change the data type and minor errors
    converted str to int and changed range from 1 to 2 to reduce time in running the loop.

--- fact.py
+++ fact.py
@@ -1,5 +1,5 @@
-n=input(("enter a value"))
- s=1
-for i in range(1,n+1):
-  s=s*i
-print("result=",s)
+n = int(input(("enter a value")))
+s=1
+for i in range(2,n+1):
+  s = s * i
+print("result = ",s)
