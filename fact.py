ccb1a94f0798e8a5588f72e96497d47d74324312
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-28 09:11:13 +0530

    Update fact.py
    did minor changes and added comments.

--- fact.py
+++ fact.py
@@ -1,5 +1,11 @@
-n = int(input(("enter a value")))
+#storing the input as int datatype given by user inn avariable.
+n = int(input(("enter a value = ")))
+#alotting another variable a value.
 s=1
+#start of loop.
 for i in range(2,n+1):
+#below is the formula used for calucation of factorial.
   s = s * i
-print("result = ",s)
+#printing the result.
+print(" The Factorial of given value is = ",s)
+#end of code.
